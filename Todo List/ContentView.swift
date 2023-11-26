763beac7a6b1555665435bd7a6dc2114fe8ad8fb
Alex Sobolev <31108@mail.ru>
2023-11-26 14:38:35 +0300

    Initial Commit

---
+++ Todo List/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Todo List
+//
+//  Created by Александр Соболев on 26.11.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
